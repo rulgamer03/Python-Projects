e90f5f8307eee896f6970af10ab8b0330f60953d
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-06-01 23:01:36 -0500

    Update operadoreslogicos.py

--- 2021learning/operadoreslogicos.py
+++ 2021learning/operadoreslogicos.py
@@ -1,15 +1,17 @@
-Negacion not(True) -> False       
-not (False) -> True               
-1. Not                            
-2. And                            
-3. Or                             
-"""                               
-a = 10                            
-b = 15                            
-c = 20                            
-resultado = ((a > b) and (b < c)) 
-print(resultado)                  
-resultado = ((a > b) or (b < c))  
-print(resultado)                  
-print(not resultado)              
-                                  
+"""                                        
+True and True -> True otro caso -> False   
+Con or minimo un True da true              
+Negacion not(True) -> False                
+not (False) -> True                        
+1. Not                                     
+2. And                                     
+3. Or                                      
+"""                                        
+a = 10                                     
+b = 15                                     
+c = 20                                     
+resultado = ((a > b) and (b < c))          
+print(resultado)                           
+resultado = ((a > b) or (b < c))           
+print(resultado)                           
+print(not resultado)                       
