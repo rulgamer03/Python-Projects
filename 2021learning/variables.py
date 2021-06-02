afc2b520438648c3952057681871635e374fffb0
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-06-01 22:26:25 -0500

    Create variables.py

---
+++ 2021learning/variables.py
@@ -0,0 +1,31 @@
+numero = 10
+flotante = 10.46
+num1 = 10
+num2 = 2
+potencia = num1 ** num2
+cadena = "Hello world"
+string = 'Hello world'
+cadenacomillas = "I am 'coding'"
+verdadero = True
+falso = False
+
+print(numero)
+print(type(numero))
+print(flotante)
+print(type(flotante))
+print(cadena)
+print(type(cadena))
+print(string)
+print(type(string))
+print(cadenacomillas)
+print(type(cadenacomillas))
+print(verdadero)
+print(type(verdadero))
+print(falso)
+print(type(falso))
+print(potencia)
+print("El resultado de ", num1, " elevado a la ", num2, " es ", potencia)
+change = 10
+print(change)
+change = "Ruly"
+print(change)
