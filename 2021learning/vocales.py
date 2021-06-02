6152277ba5ae67bc433351c73ac8f17220611c9f
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-06-02 15:27:55 -0500

    Create vocales.py

---
+++ 2021learning/vocales.py
@@ -0,0 +1,6 @@
+letra = input("Digite un caracter: ").lower()
+
+if letra=='a' or letra=='e' or letra=='i' or letra=='o' or letra=='u':
+    print("Es una vocal")
+else:
+    print("No es una vocal")
