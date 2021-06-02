df5d8099cb0da5608f6a6bad5389afd576fc9ea8
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-06-02 15:11:22 -0500

    Create condicional.py

---
+++ 2021learning/condicional.py
@@ -0,0 +1,10 @@
+# Condicionales
+
+numero = int(input("Digite un numero entero: "))
+
+if numero > 0:
+    print("El numero es positivo")
+elif numero == 0:
+    print("El numero es cero")
+else:
+    print("Es un numero negativo")
