5a654e2f959e99e461b0c1079acc3bc0708029a7
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-06-02 15:42:27 -0500

    Create playstring.py

---
+++ 2021learning/playstring.py
@@ -0,0 +1,9 @@
+string="Hello world"
+print(string[0])
+print(string)
+string=string.replace(" ", "_")
+print(string)
+lista=[string, "Hi world", "My name is ruly", "Have a fantastic day"]
+print(lista[1])
+print(lista[-1][2])
+print(lista[0:3])
