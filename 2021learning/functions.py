2ba599d0b30673be846864bc85f259993c41ce52
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-06-02 00:02:34 -0500

    Create functions.py

---
+++ 2021learning/functions.py
@@ -0,0 +1,27 @@
+n = "13"
+num = int(n)
+floa = float(n)
+print(type(num))
+print(type(floa))
+string = str(10)
+print(type(string))
+string = str(10.98)
+print(type(string))
+n = bin(10)
+print(n)
+# 0b es binario
+n = hex(100)
+print(n)
+n = int("0b1010", 2)
+print(n)
+n = int("0xa", 16)
+print(n)
+a = -9
+n = abs(a)
+print(n)
+b = 9.99891
+n = round(b)
+print(n)
+n = len("Raul")
+# Cuenta caracteres en cadena
+print(n)
