c6f976569902aae36375bb12b5dc34f2a25e8472
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-06-01 23:00:56 -0500

    Create operadoreslogicos.py

---
+++ 2021learning/operadoreslogicos.py
@@ -0,0 +1,15 @@
+Negacion not(True) -> False       
+not (False) -> True               
+1. Not                            
+2. And                            
+3. Or                             
+"""                               
+a = 10                            
+b = 15                            
+c = 20                            
+resultado = ((a > b) and (b < c)) 
+print(resultado)                  
+resultado = ((a > b) or (b < c))  
+print(resultado)                  
+print(not resultado)              
+                                  
