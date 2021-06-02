49aa74bdabb1db563310e159bd57130db6a643c1
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-06-02 15:55:27 -0500

    Update playstring.py

--- 2021learning/playstring.py
+++ 2021learning/playstring.py
@@ -8,5 +8,10 @@ print(lista[1])
 print(lista[-1][2])
 print(lista[0:3])
 stringtwo="Hello Ruly"
-stringtwo=stringtwo.replace(stringtwo[0], "_")
+stringtwo=stringtwo.replace(stringtwo[3], "_")
 print(stringtwo)
+stringfinal="Hello Ruly"
+stringfinal=stringfinal.replace("l", "_", 1)
+# El 1 limita la primera vez
+print(stringfinal)
+
