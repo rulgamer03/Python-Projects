1a2fb99340f5870918b1b7b83d34aa2415f84bd7
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-06-01 22:36:17 -0500

    Create aritmetica.py

---
+++ 2021learning/aritmetica.py
@@ -0,0 +1,13 @@
+# Aritmetica basica
+num1 = 20
+num2 = 3
+resta = num1-num2
+suma = num1+num2
+potencia = num1**num2
+division = num1/num2
+modulo = num1 % num2
+print(modulo)
+print(resta)
+print(suma)
+print(potencia)
+print(division)
