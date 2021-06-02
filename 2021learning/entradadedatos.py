d535f729db395f7eff4c5592571f9590a7be6cd7
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-06-01 23:32:17 -0500

    Create entradadedatos.py

---
+++ 2021learning/entradadedatos.py
@@ -0,0 +1,7 @@
+# Entrada de datos
+
+name = input("What is your name: ")
+year1 = int(input("What is you birth year: "))
+year2 = int(input("What is you actual year: "))
+age = year2 - year1
+print(f"Your name is {name} and you are {year2-year1} years old")
