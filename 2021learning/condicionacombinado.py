400f196c1dc52243c9dd8d053305a30158e4c09a
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-06-02 15:16:24 -0500

    Create condicionacombinado.py

---
+++ 2021learning/condicionacombinado.py
@@ -0,0 +1,14 @@
+# Condicionales combinados
+
+edad = int(input("Digite su edad: "))
+
+if edad > 0:
+    print("Edad correcta")
+    if edad >= 18:
+        print("Es mayor de edad")
+    else:
+        print("No es mayor de edad")
+
+else:
+    print("Edad incorrecta")
+    
