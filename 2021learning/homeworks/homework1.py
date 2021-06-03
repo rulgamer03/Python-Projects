ee52cb4fbe71e18735dbc9c7bfa261aeb6a1b1dc
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-06-02 22:27:35 -0500

    Update homework1.py

--- 2021learning/homeworks/homework1.py
+++ 2021learning/homeworks/homework1.py
@@ -1,12 +1,10 @@
-# Fibonacci series 
+# Fibonacci series
 position = -2  # First start with a negative number to enter in the while
 
 
 def fib(n):  # Function definition need a number n
-    if n == 0:  # if n is 0 we return 0
-        return 0
-    elif n == 1:  # else if n is 1 we return 0
-        return 1
+    if n == 0 or n == 1:  # if n is 0 we return 0
+        return n
     else:
         return fib(n - 1) + fib(n - 2)  # else we use recursivity use the function in the same function
 
