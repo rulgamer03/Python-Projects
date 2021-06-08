f9b4ae2efc0ac018db9a34b4fd1fa3d14d551613
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-06-07 22:46:22 -0500

    Create poo2.py

---
+++ 2021learning/poo2.py
@@ -0,0 +1,32 @@
+# https://www.youtube.com/watch?v=lg9p0yWgXYk&list=PLh7JzoyIyU4JVOeKkiNYPkAWBq0Aszxc-&index=2
+
+import  random
+class Zombi:
+
+    def __init__(self):
+        self.calle = random.randint(10,20)
+        self.direccion = random.choice(["izquierda", "derecha"])
+
+    def moverse(self):
+        if self.direccion == "izquierda":
+            self.calle -= random.randint(1,3)
+        else:
+            self.calle += random.randint(1,3)
+
+    def no_visible(self):
+        if self.calle < 0 or self.calle > 40:
+            return True
+        else:
+            return False
+
+z = Zombi()
+print(z.calle)
+print(z.direccion)
+z.moverse()
+print(z.calle)
+print(z.no_visible())
+
+print("----\n")
+for i in range(12): #Velocidad es random
+    print(z.calle)
+    z.moverse()
