8d64d19d00a96f475a70d6a6fee64d9212261122
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-06-07 19:34:32 -0500

    Update maximum.py

--- 2021learning/maximum.py
+++ 2021learning/maximum.py
@@ -1,4 +1,6 @@
-datos = [257, 573, -714, 321, -158, 642, 407]
+datos = [257, 573, -714, 321, -158, 642, 407, -2]
 
 print(max(datos, key = abs))
 print(max(datos))
+print(min(datos, key = abs))
+print(min(datos))
