b41660b3d23a8cbc4f1d8c0691157b1b51cd2b32
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-06-11 14:18:30 -0500

    Update SeisRegiones.py

--- SeisRegiones.py
+++ SeisRegiones.py
@@ -1,18 +1,17 @@
 filas = 6
 columnas = 12
 import random
-# import numpy as np
+import numpy as np
 matriz = []
-
-print("Registros mensuales de Temperatura")
+promedios = []
 for i in range(filas):
     matriz.append([])
     for j in range(columnas):
-        # valor = float(input(f"Region {i+1}, Mes {j+1} "))
-        valor = random.randint(10, 20)
+        valor = float(input(f"Region {i+1}, Mes {j+1} "))
         matriz[i].append(valor)
 
 print()
+print("\nRegistros mensuales de temperatura (las regiones son las filas y los meses son las columnas)\n")
 for fila in matriz:
     print ("[", end = " ")
     for elemento in fila:
@@ -26,11 +25,51 @@ for i in range(filas):
     suma = 0
     for j in range(columnas):
         suma += matriz[i][j]
-    promedio=suma/12
-    print(f"region {i+1} tuvo un promedio de {promedio}")
+    promedio=round(suma/12,2)
+    print(f"La region {i + 1} tuvo un promedio de {promedio}")
+    promedios.append([])
+    promedios[i] = promedio
+
+
+maximo = matriz[0][0]
+minimo = matriz[0][0]
+indice_maximo=(0,0)
+indice_minimo=(0,0)
+
+for i in range(filas):
+    for j in range(columnas):
+        if matriz[i][j] > maximo:
+            maximo = matriz[i][j]
+            indice_maximo = (i,j)
+        if matriz[i][j]  < minimo:
+            minimo = matriz[i][j]
+            indice_minimo = (i, j) # Tuple
 """
-print(np.amin(matriz))
-print(np.amax(matriz))
-np.where(matriz == np.amin(matriz))
-np.where(matriz == np.amax(matriz))
+print("Máximo valor de la matriz:", maximo )
+
+print("Primera posición en que aparece el máximo valor:", indice_maximo)
+
+print("Mínimo valor de la matriz:", minimo)
+
+print("Primera posición en que aparece el mínimo valor:", indice_minimo)
 """
+mes_maximo = indice_maximo[1] +1
+region_maximo = indice_maximo[0]+1
+mes_minimo = indice_minimo[1] +1
+region_minimo = indice_minimo[0] +1
+
+print(f"\nEl mes {mes_maximo} la region {region_maximo}  obtuvo la temperatura maxima de {maximo}")
+print(f"El mes {mes_minimo} la region {region_minimo}  obtuvo la temperatura minima de {minimo}")
+
+
+#print(promedios)
+region_con_mayor_promedio=0
+promedio_mayor=0
+for i in range(6):
+        if promedios[i] > promedio_mayor:
+            promedio_mayor = promedios[i]
+            region_con_mayor_promedio = i
+
+print(f"\nLa region {region_con_mayor_promedio+1} es la que tiene el mayor promedio ({promedio_mayor})")
+
+
