654bf5f224116e5bb0769d47128548f696733716
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-06-16 16:35:59 -0500

    Create json_data_filter.py

---
+++ the-complete-guide-to-bot-creation/batch_renamer/json_data_filter.py
@@ -0,0 +1,41 @@
+# Only add ?__a=1 in the link of the ig 
+
+import os
+import json
+
+# get all json files from current directory
+dir_content = os.listdir(".")
+json_files = [doc for doc in dir_content if doc.endswith("json")]
+processed = 0
+
+# get a file reference to write data to (summary.csv) and write header
+data_file = open("./summary.csv", "w")
+data_file.write("username, posts, follows, followers\n") # header
+
+for doc in json_files:
+    # open the json file and load the content into a dict
+    with open(doc, "r", encoding='utf-8') as json_file:
+        try:
+            content = json.loads(json_file.read())
+            # extra to understand better
+            
+            # something = content["seo_category_infos"]
+            # print(something)
+            # print(f"0 position {something[0]}")
+            # print(f"0 1 position {something[0][1]}")
+
+            # end extra
+            user_content = content["graphql"]["user"] # Here we have al the information that we need
+            username = user_content["username"] # in the content search the user name 
+            posts = user_content["edge_owner_to_timeline_media"]["count"]
+            follows = user_content["edge_follow"]["count"]
+            followers = user_content["edge_followed_by"]["count"]
+            # write data to file
+            data_file.write(f"{username}, {posts}, {follows}, {followers}\n")
+
+            processed += 1
+            print(f"User {username}: {posts}, {follows}, {followers}")
+        except json.decoder.JSONDecodeError as err:
+            print(f"Could not load json from {doc}... {err}")
+
+print(f"Processed {processed} of {len(json_files)}")
