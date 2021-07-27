91b519998bfb6da019327482b38b3fb6b226ecd3
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-07-26 23:12:14 -0500

    Update worksentence.py

--- worksentence.py
+++ worksentence.py
@@ -8,6 +8,15 @@ def worksentence(sentence, separator=" "):
         print()
         words.pop(-1)
 
+worksentence("La tercera forma de eliminar un eliminar elementos de una lista con Python es la sentencia del")
 
 
-worksentence("Hola como estas muy bien")
+print("--------------------------------")
+
+def worksentence2(sentence, separator=" "):
+    number_of_words = len(sentence.split(separator))
+    for j in range (number_of_words-1):
+        sentence = sentence.rsplit(' ', 1)[0]
+        print(sentence)
+
+worksentence2("La tercera forma de eliminar un eliminar elementos de una lista con Python es la sentencia del")
