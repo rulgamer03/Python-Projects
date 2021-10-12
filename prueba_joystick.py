e73594bb6629eff3187331833ca7ed53bd9fdff9
rulgamer03 <46912592+rulgamer03@users.noreply.github.com>
2021-10-12 14:21:31 -0500

    Update prueba_joystick.py

--- prueba_joystick.py
+++ prueba_joystick.py
@@ -5,7 +5,7 @@ import pygame
 from pygame.locals import *
 import time
 file1 = open("output.txt","w") 
-timeout = time.time() + 60*5   # 5 minutes from now
+timeout = time.time() + 60 * 5   # 5 minutes from now
 
 pygame.init()
 pygame.display.set_caption('game base')
@@ -16,7 +16,7 @@ pygame.joystick.init()
 joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
 for joystick in joysticks:
     print(joystick.get_name())
-    file1.write(joystick.get_name())
+    file1.write(joystick.get_name()+'\n')
 
 my_square = pygame.Rect(50, 50, 50, 50)
 my_square_color = 0
@@ -37,29 +37,28 @@ while True:
         motion[1] = 0
     my_square.x += motion[0] * 10
     my_square.y += motion[1] * 10
-
     for event in pygame.event.get():
         if event.type == JOYBUTTONDOWN:
             print(event)
-            file1.write(event)
+            file1.write(str(event)+'\n')
             if event.button == 0:
                 my_square_color = (my_square_color + 1) % len(colors)
         if event.type == JOYBUTTONUP:
             print(event)
-            file1.write(event)
+            file1.write(str(event)+'\n')
         if event.type == JOYAXISMOTION:
             print(event)
-            file1.write(event)
+            file1.write(str(event)+'\n')
             if event.axis < 2:
                 motion[event.axis] = event.value
         if event.type == JOYHATMOTION:
             print(event)
-            file1.write(event)
+            file1.write(str(event)+'\n')
         if event.type == JOYDEVICEADDED:
             joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
             for joystick in joysticks:
                 print(joystick.get_name())
-                file1.write(joystick.get_name())
+                file1.write(joystick.get_name()+'\n')
         if event.type == JOYDEVICEREMOVED:
             joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
         if event.type == QUIT:
@@ -72,3 +71,4 @@ while True:
 
     pygame.display.update()
     clock.tick(60)
+
